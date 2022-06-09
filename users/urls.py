d573d8e9aa8b2a30f1888b9f296a6ae53a13b1f5
Mohamad Hatami <hatami391998@gmail.com>
2022-06-09 10:04:06 +0700

    readi to update

--- users/urls.py
+++ users/urls.py
@@ -11,7 +11,7 @@ from .views import LoginView
 
 urlpatterns = [
     path("registration/", UserCreate.as_view()),
-    # path('', TodosListCreateView.as_view()),
+    path('', TodosListCreateView.as_view()),
     path('login', LoginView.as_view()),
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
